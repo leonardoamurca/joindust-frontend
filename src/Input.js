fa87a4d5267acfc91b6670abb9eb0a9fd38af2ef
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-05 14:57:51 -0300

    chore: add type check for input component

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Input({ placeholder, value, onChange }) {
-  return <input value={value} onChange={onChange} placeholder={placeholder}></input>
+  return <input value={value} onChange={onChange} placeholder={placeholder} />
 }
 
 Input.defaultProps = {
   value: "",
   placeholder: "",
   onChange: () => {},
+}
+
+Input.propTypes = {
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func
 }
 
 export default Input;
