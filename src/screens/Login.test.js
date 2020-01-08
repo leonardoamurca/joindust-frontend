6d621441bf5760a352e8a6ed00d488d109971431
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-07 22:11:34 -0300

    refactor: login tests

--- src/screens/Login.test.js
+++ src/screens/Login.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-//import userEvent from "@testing-library/user-event";
 import { render } from '@testing-library/react';
 import Login from './Login';
 import userEvent from '@testing-library/user-event';
 
 describe('Login', () => {
   it('should have 2 fields with value props', () => {
-    const { getByPlaceholderText } = render(<Login />);
+    const { getByPlaceholderText } = setup();
     const inputEmail = getByPlaceholderText(/e-mail/i);
     const inputPassword = getByPlaceholderText(/senha/i);
 
@@ -16,55 +15,84 @@ describe('Login', () => {
   });
 
   it('should have 1 e-mail field', () => {
-    const { getByPlaceholderText } = render(<Login />);
+    const { getByPlaceholderText } = setup();
     const input = getByPlaceholderText(/e-mail/i);
 
     expect(input).toHaveAttribute('type', 'email');
   });
 
   it('should have 1 password field', () => {
-    const { getByPlaceholderText } = render(<Login />);
+    const { getByPlaceholderText } = setup();
     const input = getByPlaceholderText(/senha/i);
 
     expect(input).toHaveAttribute('type', 'password');
   });
 
   it('should only submit when clicks on Login button and all inputs are filled', () => {
-    const { getByText, getByTestId, getByPlaceholderText } = render(<Login />);
+    const { getByText, getByTestId, getByPlaceholderText } = setup();
 
     const loginButton = getByText(/entrar/i);
     const isLoadingLabel = getByTestId(/loading/i);
     const emailField = getByPlaceholderText(/e-mail/i);
     const passwordField = getByPlaceholderText(/senha/i);
 
-    // All fields empty
-    userEvent.click(loginButton);
-
+    assertFromEmptyFields();
     expect(isLoadingLabel.textContent).toBe('');
 
-    // All fields filled
-    userEvent.type(emailField, 'leo');
-    userEvent.type(passwordField, '123');
-    userEvent.click(loginButton);
-
+    assertFromAllFieldsFilled();
     expect(isLoadingLabel.textContent).toBe('Loading...');
 
-    userEvent.type(emailField, '', { allAtOnce: true });
-    userEvent.type(passwordField, '', { allAtOnce: true });
+    resetFields();
 
-    // Only Email field filled
-    userEvent.type(emailField, 'leo@gmail.com');
-    userEvent.click(loginButton);
-
+    assertFromOnlyEmailFieldFilled();
     expect(isLoadingLabel.textContent).toBe('');
 
-    userEvent.type(emailField, '', { allAtOnce: true });
-    userEvent.type(passwordField, '', { allAtOnce: true });
+    resetFields();
 
-    // Only Password field filled
-    userEvent.type(passwordField, '123');
-    userEvent.click(loginButton);
-
+    assertFromOnlyPasswordFieldFilled();
     expect(isLoadingLabel.textContent).toBe('');
+
+    function assertFromEmptyFields() {
+      userEvent.click(loginButton);
+    }
+
+    function assertFromAllFieldsFilled() {
+      userEvent.type(emailField, 'leo');
+      userEvent.type(passwordField, '123');
+      userEvent.click(loginButton);
+    }
+
+    function assertFromOnlyEmailFieldFilled() {
+      userEvent.type(emailField, 'leo@gmail.com');
+      userEvent.click(loginButton);
+    }
+
+    function assertFromOnlyPasswordFieldFilled() {
+      userEvent.type(passwordField, '123');
+      userEvent.click(loginButton);
+    }
+
+    function resetFields() {
+      userEvent.type(emailField, '', { allAtOnce: true });
+      userEvent.type(passwordField, '', { allAtOnce: true });
+    }
   });
+
+  function setup() {
+    const {
+      getByPlaceholderText,
+      getByText,
+      queryByText,
+      getByTestId,
+      rerender,
+    } = render(<Login />);
+
+    return {
+      getByPlaceholderText,
+      getByText,
+      queryByText,
+      getByTestId,
+      rerender,
+    };
+  }
 });
