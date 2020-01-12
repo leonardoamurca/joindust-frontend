7c69b7b0f1676c51c21c1e0a2a3b81c9d422c7f2
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-12 16:42:30 -0300

    feat: adusting input and button to antd

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
+import { Input, Icon, Button } from 'antd';
 
-import Input from '../components/Input';
-import Button from '../components/Button';
 import { useAuth } from '../context/auth';
 import useCallbackStatus from '../utils/useCallbackStatus';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const { login } = useAuth();
   const { isPending, isRejected, error, run } = useCallbackStatus();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const isSubmitable = email && password;
 
@@ -20,19 +19,24 @@ function Login() {
   return (
     <div>
       <Input
+        placeholder="Digite seu e-mail"
         onChange={e => setEmail(e.target.value)}
-        placeholder="e-mail"
         value={email}
         type="email"
+        prefix={<Icon type="mail" />}
       />
-      <Input
+
+      <Input.Password
+        placeholder="Digite sua senha"
         onChange={e => setPassword(e.target.value)}
-        placeholder="senha"
         value={password}
         type="password"
+        prefix={<Icon type="lock" />}
       />
-      <Button onClick={onTryLogin} label="Entrar" />
-      {isPending && <div>Loading...</div>}
+
+      <Button loading={isPending} type="primary" onClick={onTryLogin}>
+        Entrar
+      </Button>
       {isRejected && (
         <div css={{ color: 'red' }}>{error ? error.message : null}</div>
       )}
