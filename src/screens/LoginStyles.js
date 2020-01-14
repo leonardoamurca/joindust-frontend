3ad2c213402991d1fe0f6989ae5484bb88ef9dfc
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-14 17:11:24 -0300

    feat: handling redirects and styles of login screen

---
+++ src/screens/LoginStyles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  position: static;
+  padding-top: 100px;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  height: 230px;
+  padding-bottom: 50px;
+  position: relative;
+`;
