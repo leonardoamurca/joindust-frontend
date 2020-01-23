4f170887e5caa086ebb9d0a8414366e9e55658da
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-23 01:07:38 -0300

    chore: test delete request

--- src/screens/Producer/MyCollects.js
+++ src/screens/Producer/MyCollects.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useProducer } from '../../context/producer';
 
 function MyCollects() {
   const producer = useProducer();
+
+  const deleteMe = () => {
+    return producer.deleteCollectById({collectId: 19}).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 
   return (
     <div>
       <h1>My Collects</h1>
       <ul>
         {producer.collections.content.map(collect => (
-          <li>
+          <li key={collect.id}>
             <div>Preço: R${collect.price}</div>
             <div>Quantidade: {collect.quantity}</div>
           </li>
         ))}
       </ul>
+
+      <button onClick={deleteMe}>Delete me</button>
     </div>
   );
 }
