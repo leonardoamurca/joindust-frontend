c173f490b96cc4c39cccbf14c3b1ccc93c43a09e
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-26 15:44:50 -0300

    feat: add logo v1

--- src/screens/Producer/MyCollectsStyles.js
+++ src/screens/Producer/MyCollectsStyles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  padding-bottom: 150px;
 `;
 
 export const CollectsContainer = styled.div`
