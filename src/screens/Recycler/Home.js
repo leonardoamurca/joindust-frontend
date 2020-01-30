997b160481f679a3e032bb7f25bc31d736ef3b05
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-30 19:31:09 -0300

    chore: adjusting menu and profile render

--- src/screens/Recycler/Home.js
+++ src/screens/Recycler/Home.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { useAuth } from '../../context/auth';
 import NavLink from '../../components/NavLink';
+import Menu from '../../components/Menu';
+import { Button } from 'antd';
+import Footer from '../../components/Footer';
 
 function Home({ children }) {
   const auth = useAuth();
 
   return (
     <>
-      <h1>Auth Recycler App</h1>
-      <button onClick={auth.logout}>logout</button>
-      <ul>
-        <li>
-          <NavLink to="all-collects">All Collects</NavLink>
-        </li>
-        <li>
-          <NavLink to="my-contacts">My Contacts</NavLink>
-        </li>
-        <li>
-          <NavLink to="profile">Profile</NavLink>
-        </li>
-      </ul>
+      <Menu>
+        <NavLink to="all-collects">Todas as coletas</NavLink>
+
+        <div>
+          <NavLink to="my-contacts">Meus Contatos</NavLink>
+        </div>
+        <div>
+          <NavLink to="profile">Perfil</NavLink>
+        </div>
+
+        <div style={{ position: 'absolute', bottom: '30px', left: '16px' }}>
+          <Button icon="logout" onClick={auth.logout}>
+            Sair
+          </Button>
+        </div>
+      </Menu>
       {children}
+      <Footer />
     </>
   );
 }
