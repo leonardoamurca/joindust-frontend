8e59ecdf7c2dabf9644a4cb897241d06b6364092
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-31 11:58:58 -0300

    feat: displaying formated data on MyContacts screen

--- src/screens/Recycler/Home.js
+++ src/screens/Recycler/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useAuth } from '../../context/auth';
 import NavLink from '../../components/NavLink';
 import Menu from '../../components/Menu';
 import { Button } from 'antd';
 import Footer from '../../components/Footer';
 
+const Container = styled.div`
+  position: relative;
+  min-height: 100vh;
+`;
+
 function Home({ children }) {
   const auth = useAuth();
 
   return (
-    <>
+    <Container>
       <Menu>
         <NavLink to="all-collects">Todas as coletas</NavLink>
 
@@ -28,7 +34,7 @@ function Home({ children }) {
       </Menu>
       {children}
       <Footer />
-    </>
+    </Container>
   );
 }
 
--- src/screens/Recycler/MyContacts.js
+++ src/screens/Recycler/MyContacts.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useCallbackStatus from '../../utils/useCallbackStatus';
 import { useUser } from '../../context/user';
+import { Container } from './MyContactsStyles';
+import { PageTitle } from '../../components/PageTitle';
+import { ContactsContainer } from './MyContactsStyles';
+import { Spin, message } from 'antd';
+import ContactCard from '../../components/ContactCard';
 
 function MyContacts() {
-  const user = useUser();
+  const recycler = useUser();
+  const [contacts, setContacts] = useState([]);
+  const { isPending, isRejected, error, run } = useCallbackStatus();
 
-  return <h1>My Contacts</h1>;
+  useEffect(() => {
+    run(recycler.getContacts()).then(res => {
+      setContacts(res);
+    });
+  }, []);
+
+  const onDeleteContact = async (id, corporateName) => {
+    // TODO: Change collectId to contactId in the backend
+    const res = await recycler.deleteContactById({ collectId: id });
+    if (res.collectId !== 'undefined') {
+      const filtered = contacts.content.filter(contact => contact.id !== id);
+
+      message.success(`${corporateName} removido com sucesso!`);
+      setContacts(prev => ({ ...prev, content: [...filtered] }));
+    }
+  };
+
+  return (
+    <Container>
+      <PageTitle>Meus Contatos</PageTitle>
+      <ContactsContainer>
+        {contacts.length !== 0 && contacts.content.length !== 0 ? (
+          contacts.content.map(contact => (
+            <ContactCard
+              id={contact.id}
+              key={contact.id}
+              corporateName={contact.corporateName}
+              email={contact.email}
+              phone={contact.phone}
+              onDelete={onDeleteContact}
+            />
+          ))
+        ) : isPending ? (
+          <Spin size="default" />
+        ) : (
+          <div style={{ textAlign: 'center' }}>Não há coletas cadastradas!</div>
+        )}
+      </ContactsContainer>
+    </Container>
+  );
 }
 
 export default MyContacts;
