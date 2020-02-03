733f81fc010e1cf251cc4f24c8dc9534b6af7cc3
Leonardo Murça <leonardoamurca@gmail.com>
2020-02-03 14:59:38 -0300

    feat: add profile image uploader

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { createRef } from 'react';
+import { Avatar, Spin, message } from 'antd';
+
 import {
   Container,
   CorporateContainer,
@@ -11,16 +13,23 @@ import {
   DataLabel,
   Data,
   DataContainer,
+  AvatarContainer,
 } from './ProfileStyles';
-import { Avatar, Spin } from 'antd';
+
 import { useAuth } from '../context/auth';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { isProducer } from '../utils/constants';
+import { uploadImage } from '../services/auth-client';
+import toBase64 from '../utils/toBase64';
+import compressImage from '../utils/compressImage';
 
 function Profile() {
   const auth = useAuth();
   const [profile, setProfile] = useState(null);
+  const [successFeedback, setSuccessFeedback] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const inputRef = createRef();
 
   useEffect(() => {
     auth.getUserProfile({ username: auth.data.user.username }).then(res => {
@@ -28,16 +37,79 @@ function Profile() {
     });
   }, []);
 
+  const onClickUploadFile = () => {
+    inputRef.current.click();
+  };
+
+  const onChangeProfileImage = async e => {
+    const file = e.target.files[0];
+
+    try {
+      setIsLoading(true);
+      const base64File = await toBase64(file);
+      const compressedImage = await compressImage(base64File);
+      const uploadedImage = await uploadImage({
+        profileImage: compressedImage,
+      });
+
+      if (uploadedImage && uploadedImage.success) {
+        setIsLoading(false);
+        const res = await auth.updateProfileImage({
+          profileImage: uploadedImage.link,
+        });
+
+        if (res) {
+          auth
+            .getUserProfile({ username: auth.data.user.username })
+            .then(response => {
+              setProfile(response);
+              setSuccessFeedback(res);
+            });
+        }
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  const renderSuccessFeedback = () => {
+    return message.success(successFeedback.data.message);
+  };
+
   return (
     <Container>
       {profile ? (
         <>
-          <Avatar
-            src={profile.profileImage}
-            shape="circle"
-            size={100}
-            icon="user"
-          />
+          <AvatarContainer onClick={onClickUploadFile}>
+            {isLoading && (
+              <Spin
+                size="default"
+                style={{
+                  zIndex: '200',
+                  left: '40px',
+                  top: '40px',
+                  position: 'absolute',
+                }}
+              />
+            )}
+
+            <Avatar
+              src={profile.profileImage}
+              shape="circle"
+              size={100}
+              icon="user"
+            />
+            <input
+              ref={inputRef}
+              onChange={onChangeProfileImage}
+              multiple={false}
+              style={{ display: 'none' }}
+              type="file"
+              accept="image/png, image/jpeg"
+            />
+            {successFeedback && renderSuccessFeedback()}
+          </AvatarContainer>
           <CorporateContainer>
             <CorporateName>{profile.corporateName}</CorporateName>
             <CorporateRole>
